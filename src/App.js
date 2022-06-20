f4bb1c5d7d84c432bbff9aa2bb05f2932426e9b8
David Martinez <anddagger@gmail.com>
2022-06-19 22:17:13 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+const App = () => {
+  
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    )
+  
+}
+
+export default App
